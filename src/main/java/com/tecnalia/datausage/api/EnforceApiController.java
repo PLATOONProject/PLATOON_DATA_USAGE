b0d73f99e353dc62ca3028d02c19df1ac5f32785
usuario <usuario@localhost>
2021-04-07 12:22:14 +0200

    Quitar código del servicio REST y ponerlo en otra clase

--- src/main/java/com/tecnalia/datausage/api/EnforceApiController.java
+++ src/main/java/com/tecnalia/datausage/api/EnforceApiController.java
@@ -2,17 +2,7 @@ package com.tecnalia.datausage.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tecnalia.datausage.model.IdsUseObject;
-import com.tecnalia.datausage.model.ContractStore;
-import com.tecnalia.datausage.model.RuleStore;
-import com.tecnalia.datausage.repository.ContractRepository;
-import com.tecnalia.datausage.repository.RuleRepository;
-import de.fraunhofer.iais.eis.Contract;
-import de.fraunhofer.iais.eis.Permission;
-import de.fraunhofer.iais.eis.Prohibition;
-import de.fraunhofer.iais.eis.Rule;
-import de.fraunhofer.iais.eis.ids.jsonld.Serializer;
-import de.fraunhofer.isst.dataspaceconnector.exceptions.contract.UnsupportedPatternException;
-import de.fraunhofer.isst.dataspaceconnector.services.usagecontrol.PolicyHandler;
+import com.tecnalia.datausage.service.EnforcementService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
@@ -40,15 +30,6 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.validation.constraints.*;
 import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-03-11T13:39:14.709Z[GMT]")
 @RestController
@@ -61,14 +42,8 @@ public class EnforceApiController implements EnforceApi {
     private final HttpServletRequest request;
 
     @Autowired
-    private  ContractRepository contractRepository;
+    private  EnforcementService enforcementService;
     
-    @Autowired    
-    private RuleRepository ruleRepository;
-
-    @Autowired 
-    PolicyHandler policyHandler;
-
     @org.springframework.beans.factory.annotation.Autowired
     public EnforceApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
@@ -78,75 +53,7 @@ public class EnforceApiController implements EnforceApi {
     public ResponseEntity<Object> usageControlUseUsingPOST(@Parameter(in = ParameterIn.DEFAULT, description = "", schema=@Schema()) @Valid @RequestBody IdsUseObject body) {
         String accept = request.getHeader("Accept");
 
-        //Get contracts from ContractAgreement table applied to this providerURI, consumerUri
-        Iterable<ContractStore> contractList = this.contractRepository.findAllByProviderIdAndConsumerId(body.getProviderUri(), body.getConsumerUri());
-        //Get contracts which start-end dates are valid according to current date, and get the most recent Contract
-        Contract validContract = null;
-        Date latestContractDate = null;
-        Date validContractStart = null;
-        String validContractUuid = null;
-        for (ContractStore contractStore: contractList) {
-            String contractTxt = contractStore.getContractAsString();
-            Serializer serializer = new Serializer();
-            try {
-                Contract contract = serializer.deserialize(contractTxt, Contract.class);
-                Date contractStart = contract.getContractStart().toGregorianCalendar().getTime();
-                Date contractEnd = contract.getContractEnd().toGregorianCalendar().getTime();
-                Date contractDate = contract.getContractDate().toGregorianCalendar().getTime();
-                Date date = new Date();
-                if (date.after(contractStart) && date.before(contractEnd)) {
-                    if((latestContractDate == null) || 
-                            ((latestContractDate != null) && (contractDate.after(latestContractDate)))){
-                        latestContractDate = contractDate;
-                        validContractStart = contractStart;
-                        validContract = contract;
-                        validContractUuid = contractStore.getContractUuid();
-                    }
-                } 
-            } catch (Exception e) {
-                return new ResponseEntity<>("Incorrect contract format", HttpStatus.BAD_REQUEST);
-            }
-        }
-        if(validContract == null)
-            return new ResponseEntity<>("No valid contracts found", HttpStatus.BAD_REQUEST);
-        
-        //Get rules from rule table applied to the contract, targetDataUri
-        Iterable<RuleStore> ruleList = this.ruleRepository.findAllByContractUuidAndTargetId(validContractUuid, body.getTargetDataUri());
-        //Classify Rules into Permissions and Prohibitions
-        ArrayList<Permission> permissionList = new ArrayList<>();
-        ArrayList<Prohibition> prohibitionList = new ArrayList<>();
-        for (RuleStore ruleStore: ruleList) {
-            String ruleTxt = ruleStore.getRuleContent();
-            Serializer serializer = new Serializer();
-            try {
-                Rule rule = serializer.deserialize(ruleTxt, Rule.class);
-                if(rule.getClass().getSimpleName().compareToIgnoreCase("Permission") == 0)
-                    permissionList.add((Permission)rule);
-                else if(rule.getClass().getSimpleName().compareToIgnoreCase("Prohibition") == 0)
-                    prohibitionList.add((Prohibition)rule);
-             } catch (Exception e) {
-                return new ResponseEntity<>("Invalid rule format", HttpStatus.BAD_REQUEST);
-            }
-        }
-        
-        try {
-            boolean allowAccess = false;
-            if(body.isConsuming()) {
-               //For each rule, apply enforcement
-               allowAccess = policyHandler.onDataAccess(permissionList, prohibitionList, validContractStart, body.getTargetDataUri(), body.getConsumerUri());               
-            } else {
-               //For each rule, apply enforcement            
-               allowAccess = policyHandler.onDataProvision(permissionList, prohibitionList, body.getConsumerUri());
-            }
-            
-            if(allowAccess) {
-                return new ResponseEntity<>(body.getDataObject(), HttpStatus.OK); 
-            } else {
-                return new ResponseEntity<>("PDP decided to inhibit the usage: Event is not allowed according to policy", HttpStatus.FORBIDDEN); 
-            }
-        } catch (UnsupportedPatternException e) {
-            return new ResponseEntity<>("Unsupported Policy Pattern", HttpStatus.BAD_REQUEST);                      
-        }
+        return enforcementService.enforce(body);
     }
 
 }
---
+++ src/main/java/com/tecnalia/datausage/service/EnforcementService.java
@@ -0,0 +1,152 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tecnalia.datausage.service;
+
+import com.tecnalia.datausage.model.IdsUseObject;
+import com.tecnalia.datausage.model.ContractStore;
+import com.tecnalia.datausage.model.RuleStore;
+import com.tecnalia.datausage.repository.ContractRepository;
+import com.tecnalia.datausage.repository.RuleRepository;
+import de.fraunhofer.iais.eis.Contract;
+import de.fraunhofer.iais.eis.Permission;
+import de.fraunhofer.iais.eis.Prohibition;
+import de.fraunhofer.iais.eis.Rule;
+import de.fraunhofer.iais.eis.ids.jsonld.Serializer;
+import de.fraunhofer.isst.dataspaceconnector.exceptions.contract.UnsupportedPatternException;
+import de.fraunhofer.isst.dataspaceconnector.services.usagecontrol.PolicyHandler;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author usuario
+ */
+@Service
+public class EnforcementService {
+    private static final Logger log = LoggerFactory.getLogger(EnforcementService.class);
+
+   @Autowired
+    private  ContractRepository contractRepository;
+    
+    @Autowired    
+    private RuleRepository ruleRepository;
+
+    @Autowired 
+    PolicyHandler policyHandler;
+
+    public ResponseEntity<Object> enforce(IdsUseObject body) {
+        //Get contracts from ContractAgreement table applied to this providerURI, consumerUri
+        Iterable<ContractStore> contractList = this.contractRepository.findAllByProviderIdAndConsumerId(body.getProviderUri(), body.getConsumerUri());
+        //Get contracts which start-end dates are valid according to current date, and get the most recent Contract
+        ContractStore validContractStore = getValidContracts(contractList);
+        if(validContractStore == null)
+            return new ResponseEntity<>("No valid contracts found", HttpStatus.BAD_REQUEST);
+        String contractTxt = validContractStore.getContractAsString();
+        Date validContractStart = null;
+        try {
+            Serializer serializer = new Serializer();
+            Contract contract = serializer.deserialize(contractTxt, Contract.class);
+            validContractStart = contract.getContractStart().toGregorianCalendar().getTime();
+        } catch (Exception e) {
+            return new ResponseEntity<>("Incorrect contract format", HttpStatus.BAD_REQUEST);
+        }
+        
+        //Get rules from rule table applied to the contract, targetDataUri
+        Iterable<RuleStore> ruleList = this.ruleRepository.findAllByContractUuidAndTargetId(validContractStore.getContractUuid(), body.getTargetDataUri());
+        //Classify Rules into Permissions and Prohibitions
+        ArrayList<Permission> permissionList = new ArrayList<>();
+        ArrayList<Prohibition> prohibitionList = new ArrayList<>();
+        for (RuleStore ruleStore: ruleList) {
+            String ruleTxt = ruleStore.getRuleContent();
+            Serializer serializer = new Serializer();
+            try {
+                Rule rule = serializer.deserialize(ruleTxt, Rule.class);
+                if(rule.getClass().getSimpleName().compareToIgnoreCase("Permission") == 0)
+                    permissionList.add((Permission)rule);
+                else if(rule.getClass().getSimpleName().compareToIgnoreCase("Prohibition") == 0)
+                    prohibitionList.add((Prohibition)rule);
+             } catch (Exception e) {
+                return new ResponseEntity<>("Invalid rule format", HttpStatus.BAD_REQUEST);
+            }
+        }
+        
+        try {
+            boolean allowAccess = false;
+            if(body.isConsuming()) {
+               //For each rule, apply enforcement
+               allowAccess = policyHandler.onDataAccess(permissionList, prohibitionList, validContractStart, body.getTargetDataUri(), body.getConsumerUri());
+               if(allowAccess)
+                   incrementAccessFrequency(body.getTargetDataUri(), body.getConsumerUri());
+            } else {
+               //For each rule, apply enforcement            
+               allowAccess = policyHandler.onDataProvision(permissionList, prohibitionList, body.getConsumerUri());
+            }
+            
+            if(allowAccess) {
+                return new ResponseEntity<>(body.getDataObject(), HttpStatus.OK); 
+            } else {
+                return new ResponseEntity<>("PDP decided to inhibit the usage: Event is not allowed according to policy", HttpStatus.FORBIDDEN); 
+            }
+        } catch (UnsupportedPatternException e) {
+            return new ResponseEntity<>("Unsupported Policy Pattern", HttpStatus.BAD_REQUEST);                      
+        }
+    }
+    
+    ContractStore getValidContracts(Iterable<ContractStore> contractList) {
+        //Get contracts which start-end dates are valid according to current date, and get the most recent Contract
+        ContractStore validContractStore = null;
+        Date latestContractDate = null;
+        for (ContractStore contractStore: contractList) {
+            String contractTxt = contractStore.getContractAsString();
+            Serializer serializer = new Serializer();
+            try {
+                Contract contract = serializer.deserialize(contractTxt, Contract.class);
+                Date contractStart = contract.getContractStart().toGregorianCalendar().getTime();
+                Date contractEnd = contract.getContractEnd().toGregorianCalendar().getTime();
+                Date contractDate = contract.getContractDate().toGregorianCalendar().getTime();
+                Date date = new Date();
+                if (date.after(contractStart) && date.before(contractEnd)) {
+                    if((latestContractDate == null) || 
+                            ((latestContractDate != null) && (contractDate.after(latestContractDate)))){
+                        validContractStore = contractStore;
+                    }
+                } 
+            } catch (Exception e) {
+            }
+        }
+        return validContractStore;
+    }
+    
+    @Transactional
+    void incrementAccessFrequency(String targetDataUri, String consumerUri) {
+        //TODO: Increment by 1 the access frequency
+        /*
+        Optional<ContractStore> bCheckExistsTarget = this.contractRepository.findByContractId(targetDataUri, consumerUri);
+        //if exists update
+        if (bCheckExistsTarget.isPresent()) {
+            contractStore = (ContractStore)bCheckExistsContract.get();
+            contractStore.setContractAsString(contractStore,getXX() + 1);
+            this.contractRepository.saveAndFlush(contractStore);                              
+        } else {
+        //insert 
+            contractStore = new ContractStore();
+            contractStore.setContractId(contract.getId().toString());
+            contractStore.setContractAsString(policy);
+            contractStore.setConsumerId(contract.getConsumer().toString());
+            this.contractRepository.saveAndFlush(contractStore);
+        }
+*/
+        return;        
+    }
+}
