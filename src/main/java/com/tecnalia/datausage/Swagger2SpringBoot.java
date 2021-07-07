34c370e7905f8cdcbb45b89c902346adb50d2ef2
root <root@platoon-VirtualBox>
2021-07-07 12:28:09 +0200

    Changes generate swagger-ui and put Authorize security-schemes

--- src/main/java/com/tecnalia/datausage/Swagger2SpringBoot.java
+++ src/main/java/com/tecnalia/datausage/Swagger2SpringBoot.java
@@ -1,37 +1,68 @@
 package com.tecnalia.datausage;
 
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Properties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-import springfox.documentation.oas.annotations.EnableOpenApi;
+
 
 @SpringBootApplication
-@EnableOpenApi
+
 @ComponentScan(basePackages = { "com.tecnalia.datausage", "com.tecnalia.datausage.api" , "com.tecnalia.datausage.configuration", 
     "com.tecnalia.datausage.utils", "de.fraunhofer.isst.dataspaceconnector.services.usagecontrol", "io.dataspaceconnector.services.usagecontrol"})
-public class Swagger2SpringBoot implements CommandLineRunner {
+public class Swagger2SpringBoot {
 
-    @Override
-    public void run(String... arg0) throws Exception {
-        if (arg0.length > 0 && arg0[0].equals("exitcode")) {
-            throw new ExitException();
-        }
-    }
+
 
     public static void main(String[] args) throws Exception {
         new SpringApplication(Swagger2SpringBoot.class).run(args);
     }
 
-    class ExitException extends RuntimeException implements ExitCodeGenerator {
-        private static final long serialVersionUID = 1L;
+     @Bean
+    public OpenAPI openApi(@Value("${server.servlet.context-path}") String contextPath,
+			@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuerUri) {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Platoon Data Usage")
+                .description("Api Documentation")
+                .termsOfService("")
+                .version("1.0")
+                .license(new License()
+                    .name("Apache 2.0")
+                    .url("http://www.apache.org/licenses/LICENSE-2.0"))
+                .contact(new io.swagger.v3.oas.models.info.Contact()
+                    .email("")))    
+                .addServersItem(new Server().url(contextPath))
+				.components(new Components()
+						.addSecuritySchemes("openid-connect",
+								new SecurityScheme().type(SecurityScheme.Type.OPENIDCONNECT)
+										.openIdConnectUrl(issuerUri + "/.well-known/openid-configuration"))
+						.addSecuritySchemes("bearer-jwt",
+								new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer")
+										.bearerFormat("JWT")))
+				.addSecurityItem(new SecurityRequirement().addList("bearer-jwt", Arrays.asList("read", "write")))
+				.addSecurityItem(new SecurityRequirement().addList("openid-connect", Arrays.asList("read", "write")));
+	}
 
-        @Override
-        public int getExitCode() {
-            return 10;
-        }
-
-    }
+            
+    
+    
+    
 }
