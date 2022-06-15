8e5cfa108908bcdbf49a9bbbef376e2f2392ae44
usuario <usuario@kubernetes-worker>
2022-06-15 16:05:34 +0200

    Change to remove ssl and keycloak

--- src/main/java/com/tecnalia/datausage/Swagger2SpringBoot.java
+++ src/main/java/com/tecnalia/datausage/Swagger2SpringBoot.java
@@ -25,7 +25,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 
-@ComponentScan(basePackages = { "com.tecnalia.datausage", "com.tecnalia.datausage.api" , "com.tecnalia.datausage.configuration", 
+@ComponentScan(basePackages = { "com.tecnalia.datausage", "com.tecnalia.datausage.api", 
     "com.tecnalia.datausage.utils", "io.dataspaceconnector.services.usagecontrol"})
 public class Swagger2SpringBoot {
 
@@ -35,34 +35,7 @@ public class Swagger2SpringBoot {
         new SpringApplication(Swagger2SpringBoot.class).run(args);
     }
 
-     @Bean
-    public OpenAPI openApi(@Value("${server.servlet.context-path}") String contextPath,
-			@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuerUri) {
-        return new OpenAPI()
-            .info(new Info()
-                .title("Platoon Data Usage")
-                .description("Api Documentation")
-                .termsOfService("")
-                .version("1.0")
-                .license(new License()
-                    .name("Apache 2.0")
-                    .url("http://www.apache.org/licenses/LICENSE-2.0"))
-                .contact(new io.swagger.v3.oas.models.info.Contact()
-                    .email("")))    
-                .addServersItem(new Server().url(contextPath))
-				.components(new Components()
-						.addSecuritySchemes("openid-connect",
-								new SecurityScheme().type(SecurityScheme.Type.OPENIDCONNECT)
-										.openIdConnectUrl(issuerUri + "/.well-known/openid-configuration"))
-						.addSecuritySchemes("bearer-jwt",
-								new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer")
-										.bearerFormat("JWT")))
-				.addSecurityItem(new SecurityRequirement().addList("bearer-jwt", Arrays.asList("read", "write")))
-				.addSecurityItem(new SecurityRequirement().addList("openid-connect", Arrays.asList("read", "write")));
-	}
-
-            
-    
+   
     
     
 }
